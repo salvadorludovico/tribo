725945176be36c94ad05ed0cd934b0810ab1fe9e
salvadorludovico <49698940+salvadorludovico@users.noreply.github.com>
2026-03-10 13:46:07 -0300

    feat: add database instance info to status endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,6 @@
 import { Client } from "pg";
 
-async function query(queryObject) {
+async function query(queryText, queryValues) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -8,10 +8,15 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    await client.connect();
+    const result = await client.query(queryText, queryValues);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,43 @@
 import database from "infra/database.js";
 
-async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+const dbName = process.env.POSTGRES_DB;
 
-  response.status(200).json({ chave: "valor" });
+function extractQueryResult(result) {
+  return Object.values(result.rows[0]);
+}
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+  const resultVersion = await database.query("SHOW server_version;");
+  const resultVersionInfo = await database.query("SELECT VERSION();");
+  const resultMaxConnections = await database.query(
+    "SELECT CURRENT_SETTING('max_connections')::int;",
+  );
+  const resultActiveConnections = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1 AND state = $2;",
+    [dbName, "active"],
+  );
+  const resultIdleConnections = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1 AND state = $2;",
+    [dbName, "idle"],
+  );
+
+  const versionInfo = extractQueryResult(resultVersionInfo)[0];
+  const versionNumber = parseInt(extractQueryResult(resultVersion));
+  const maxConnections = extractQueryResult(resultMaxConnections)[0];
+  const activeConnections = extractQueryResult(resultActiveConnections)[0];
+  const idleConnections = extractQueryResult(resultIdleConnections)[0];
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      version_info: versionInfo,
+      version_number: versionNumber,
+      max_connections: maxConnections,
+      active_connections: activeConnections,
+      idle_connections: idleConnections,
+    },
+  });
 }
 
 export default status;
